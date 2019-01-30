faf14a19afea979f324249111d524cca08e6bb5a
VictorPadovan1997 <victorpadovan1997@gmail.com>
2019-01-30 16:55:26 -0200

    TDD
    TDD

---
+++ fizzbuzz.php
@@ -0,0 +1,11 @@
+<?php
+class FizzBuzz {
+
+    public $numero = null;
+    
+    public function __construct($numero = null) {
+        $this->numero = $numero;
+    }
+}
+
+?>
---
+++ fizzbuzzTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+Passo 1 : Testar se a classe existe.
+Passo 2 : Verificar se esta recebedendo as informações 
+Passo 3 : Testar onde for dividido 3 ele retornar Fizz.
+Passo 4 : Testar onde for dividido 5 ele retornar Buzz.
+Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz.
+Passo 6 : Caso não for divisivel por ninguem retornar o numero.
+ **/
+require_once('fizzbuzz.php');
+
+class fizzbuzzTest extends PHPUnit_Framework_TestCase {
+    
+    public function testExisteFizzBuzz(){
+        $fizzbuzz = new FizzBuzz();
+        $existe = is_a($fizzbuzz, 'FizzBuzz');
+        $this->assertEquals(true, $existe);
+    }
+
+    public function testRecebendo() {
+        $fizzbuzz = new FizzBuzz(3);
+        $this->assertEquals(3, $fizzbuzz->numero);
+    }
+}
+
+?>
