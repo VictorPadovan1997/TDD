48e33f8d7f394622b47523f5f4a82ae2ca0d2b5e
heron03 <heronze@gmail.com>
2019-02-12 11:05:25 -0200

    tdd - heron

--- fizzbuzzTest.php
+++ fizzbuzzTest.php
@@ -8,7 +8,6 @@ Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz.
 Passo 6 : Caso não for divisivel por ninguem retornar o numero.
  **/
 require_once('fizzbuzz.php');
-
 class fizzbuzzTest extends PHPUnit_Framework_TestCase {
     
     public function testExisteFizzBuzz(){
@@ -16,11 +15,25 @@ class fizzbuzzTest extends PHPUnit_Framework_TestCase {
         $existe = is_a($fizzbuzz, 'FizzBuzz');
         $this->assertEquals(true, $existe);
     }
-
     public function testRecebendo() {
-        $fizzbuzz = new FizzBuzz(3);
-        $this->assertEquals(3, $fizzbuzz->numero);
+        $fizzbuzz = new FizzBuzz(1);
+        $this->assertEquals(1, $fizzbuzz->numero);
+    }
+    public function testefizz(){
+    $fizzbuzz = new FizzBuzz(3);
+    $this->assertEquals("Fizz", $fizzbuzz->fizz());
+    }
+    public function testebuzz(){
+        $fizzbuzz = new FizzBuzz(5);
+        $this->assertEquals("Buzz", $fizzbuzz->buzz());
+    }
+    public function testefizzbuzz(){
+        $fizzbuzz = new FizzBuzz(15);
+        $this->assertEquals("FizzBuzz", $fizzbuzz->fizzbuzz());
+    }
+    public function testenumero(){
+        $fizzbuzz = new FizzBuzz(1);
+        $this->assertEquals(1, $fizzbuzz->numero());
     }
 }
-
 ?>
