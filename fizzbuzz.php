9ddb4bdcb3676fab190cc4e80d6bdc8201e19b17
heron03 <heronze@gmail.com>
2019-02-12 10:51:13 -0200

    TDD vscode Heron

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,33 +1,29 @@
 <?php
-class FizzBuzz {
-    public $numero = null;
-    
-    public function __construct($numero = null) {
-        $this->numero = $numero;
-    }
-    public function fizz($numero = null)
-     {
-     	if($this->numero % 3 == 0) $fizzBuzz = 'Fizz';{
- 		return $fizzBuzz;
- 		}
-     } 
-    public function buzz($numero = null)
-     {
-     	if($this->numero % 5 == 0) $fizzBuzz = 'Buzz';{
- 		return $fizzBuzz;
- 		}
-     }  
-     public function fizzbuzz($numero = null)
-     {
-     	if(($this->numero % 5 && $this->numero %3) == 0) $fizzBuzz = 'FizzBuzz';{
- 		return $fizzBuzz;
- 		}
-     } 
-     public function numero($numero = null)
-     {
-     	if(($this->numero % 5 && $this->numero %3) != 0) $fizzBuzz = $this->numero;{
- 		return $fizzBuzz;
- 		}
-     } 
-}
+  class FizzBuzz {
+      public $numero = null;
+      
+      public function __construct($numero = null) {
+          $this->numero = $numero;
+      }
+      public function fizz($numero = null){
+        if($this->numero % 3 == 0) $fizzBuzz = 'Fizz';{
+          return $fizzBuzz;
+        }
+      } 
+      public function buzz($numero = null){
+        if($this->numero % 5 == 0) $fizzBuzz = 'Buzz';{
+          return $fizzBuzz;
+        }
+      }  
+      public function fizzbuzz($numero = null){
+        if(($this->numero % 5 && $this->numero %3) == 0) $fizzBuzz = 'FizzBuzz';{
+          return $fizzBuzz;
+        }
+      } 
+      public function numero($numero = null){
+        if(($this->numero % 5 && $this->numero %3) != 0) $fizzBuzz = $this->numero;{
+          return $fizzBuzz;
+        }
+      } 
+  }
 ?>
--- fizzbuzzTest.php
+++ fizzbuzzTest.php
@@ -1,39 +1,39 @@
 <?php
-/**
-Passo 1 : Testar se a classe existe.
-Passo 2 : Verificar se esta recebedendo as informações 
-Passo 3 : Testar onde for dividido 3 ele retornar Fizz.
-Passo 4 : Testar onde for dividido 5 ele retornar Buzz.
-Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz.
-Passo 6 : Caso não for divisivel por ninguem retornar o numero.
- **/
-require_once('fizzbuzz.php');
-class fizzbuzzTest extends PHPUnit_Framework_TestCase {
-    
-    public function testExisteFizzBuzz(){
-        $fizzbuzz = new FizzBuzz();
-        $existe = is_a($fizzbuzz, 'FizzBuzz');
-        $this->assertEquals(true, $existe);
+    /**
+    Passo 1 : Testar se a classe existe.
+    Passo 2 : Verificar se esta recebedendo as informações 
+    Passo 3 : Testar onde for dividido 3 ele retornar Fizz.
+    Passo 4 : Testar onde for dividido 5 ele retornar Buzz.
+    Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz.
+    Passo 6 : Caso não for divisivel por ninguem retornar o numero.
+    **/
+    require_once('fizzbuzz.php');
+    class fizzbuzzTest extends PHPUnit_Framework_TestCase {
+        
+        public function testExisteFizzBuzz(){
+            $fizzbuzz = new FizzBuzz();
+            $existe = is_a($fizzbuzz, 'FizzBuzz');
+            $this->assertEquals(true, $existe);
+        }
+        public function testRecebendo() {
+            $fizzbuzz = new FizzBuzz(1);
+            $this->assertEquals(1, $fizzbuzz->numero);
+        }
+        public function testefizz(){
+            $fizzbuzz = new FizzBuzz(3);
+            $this->assertEquals("Fizz", $fizzbuzz->fizz());
+        }
+        public function testebuzz(){
+            $fizzbuzz = new FizzBuzz(5);
+            $this->assertEquals("Buzz", $fizzbuzz->buzz());
+        }
+        public function testefizzbuzz(){
+            $fizzbuzz = new FizzBuzz(15);
+            $this->assertEquals("FizzBuzz", $fizzbuzz->fizzbuzz());
+        }
+        public function testenumero(){
+            $fizzbuzz = new FizzBuzz(1);
+            $this->assertEquals(1, $fizzbuzz->numero());
+        }
     }
-    public function testRecebendo() {
-        $fizzbuzz = new FizzBuzz(1);
-        $this->assertEquals(1, $fizzbuzz->numero);
-    }
-    public function testefizz(){
-    $fizzbuzz = new FizzBuzz(3);
-    $this->assertEquals("Fizz", $fizzbuzz->fizz());
-    }
-    public function testebuzz(){
-        $fizzbuzz = new FizzBuzz(5);
-        $this->assertEquals("Buzz", $fizzbuzz->buzz());
-    }
-    public function testefizzbuzz(){
-        $fizzbuzz = new FizzBuzz(15);
-        $this->assertEquals("FizzBuzz", $fizzbuzz->fizzbuzz());
-    }
-    public function testenumero(){
-        $fizzbuzz = new FizzBuzz(1);
-        $this->assertEquals(1, $fizzbuzz->numero());
-    }
-}
 ?>
