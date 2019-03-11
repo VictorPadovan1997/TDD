cbee719f05a1b8e4a86b0ac6d907b9b824017796
VictorPadovan1997 <victorpadovan1997@gmail.com>
2019-03-11 08:47:19 -0300

    TDD

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -6,6 +6,20 @@ class FizzBuzz {
     public function __construct($numero = null) {
         $this->numero = $numero;
     }
+
+
+    public function fizzBuzz($numero = null){
+    	if ($this->numero % 3 == 0) $fizzBuzz = 'fizz';{
+    		return $fizzBuzz;
+      }
+    } 
+
+
+    public function fizzbuzz2($numero = null){
+    	if ($this->numero % 5 == 0) $fizzBuzz = 'Buzz';{
+        	return $fizzBuzz;
+      }
+    }  
 }
 
 ?>
--- fizzbuzzTest.php
+++ fizzbuzzTest.php
@@ -2,10 +2,10 @@
 /**
 Passo 1 : Testar se a classe existe.
 Passo 2 : Verificar se esta recebedendo as informações 
-Passo 3 : Testar onde for dividido 3 ele retornar Fizz.
-Passo 4 : Testar onde for dividido 5 ele retornar Buzz.
-Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz.
-Passo 6 : Caso não for divisivel por ninguem retornar o numero.
+Passo 3 : Testar onde for dividido 3 ele retornar Fizz. //VICTOR
+Passo 4 : Testar onde for dividido 5 ele retornar Buzz. //hERON
+Passo 5 : Testar onde for dividido 3 e 5 ele retornar FIZBuzz. //jOISE
+Passo 6 : Caso não for divisivel por ninguem retornar o numero. //dudu
  **/
 require_once('fizzbuzz.php');
 
@@ -21,6 +21,14 @@ class fizzbuzzTest extends PHPUnit_Framework_TestCase {
         $fizzbuzz = new FizzBuzz(3);
         $this->assertEquals(3, $fizzbuzz->numero);
     }
+
+    public function testbuzz() {
+        $fizzbuzz = new FizzBuzz(5);
+        $this->assertEquals(5, $fizzbuzz->numero);
+    }
+    
 }
+
+
 
 ?>
